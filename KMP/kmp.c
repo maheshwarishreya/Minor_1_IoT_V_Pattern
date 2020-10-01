3c2739f965440a37bb90d62ce5df911f1bebeebd
Shreya Maheshwari <maheshwarishreya99@gmail.com>
2020-10-01 13:30:45 +0530

    KMP code

---
+++ KMP/kmp.c
@@ -0,0 +1,62 @@
+#include<stdio.h> 
+#include<string.h>
+void computeLPS(char* pat,int m,int* lps);
+
+void kmp_search(char* pat,char* text){
+    int m=strlen(pat);
+    int n=strlen(text);
+    int lps[m];
+    computeLPS(pat,m,lps);
+    int i=0;
+    int j=0;
+    int res=0;
+    while(i<n && j<m){
+        if(text[i]==pat[j]){
+            i++;
+            j++;
+        }
+        else{
+            if(j>0){
+                j=lps[j-1];
+            }
+            else{
+                i++;
+            }
+        }
+    }
+    if(j==m){
+       printf("pattern present");
+    }
+    else
+    {
+        printf("pattern doesnot exist");
+    }
+    
+}
+void computeLPS(char* pat,int m,int* lps){ 
+    int i = 1; 
+    lps[0] = 0;  
+    int j=0;
+    while (i < m){ 
+        if (pat[i] == pat[j]){ 
+            if(j!=0){
+                j=lps[j-1];
+            }
+            else{
+                lps[i]=0;
+                i++;
+            }
+        }
+        else{
+            lps[i]=(j+1);
+            i++;
+            j++;
+        } 
+    } 
+}
+int main(){
+    char text[]="Hello I am shreya!!";
+    char pat[]="q";
+    kmp_search(pat,text);
+    return 0;
+}
